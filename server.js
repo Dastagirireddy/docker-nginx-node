9a659b26a22329e24ead9b2475a6852d940e156d
dastagirireddy <dastagirireddy99.r@gmail.com>
2016-11-30 22:03:43 +0530

    redis

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var app = express();
+var express = require('express'),
+    redis = require('redis'),
+    mongoose = require('mongoose');
+
 var url = process.env.MONGODB;
+var app = express();
 
 app.use(express.static(__dirname));
 
@@ -17,7 +19,22 @@ mongoose.connect(url, function(err, conn) {
     console.log("Success");
 });
 
-app.listen(3000, function() {
+console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
-    console.log("Listening on port 3000");
+// APPROACH 1: Using environment variables created by Docker
+// var client = redis.createClient(
+// 	process.env.REDIS_PORT_6379_TCP_PORT,
+//   	process.env.REDIS_PORT_6379_TCP_ADDR
+// );
+
+// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
+var client = redis.createClient('6379', 'redis');
+
+client.incr('counter', function(err, counter) {
+    if (err) return console.log(err);
+    connect.log('This page has been viewed ' + counter + ' times!');
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port ' + (process.env.PORT || 3000));
 });
