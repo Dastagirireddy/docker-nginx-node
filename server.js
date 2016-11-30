22a90c3bff07bd18fe5ca5a792c86a394bf0f196
dastagirireddy <dastagirireddy99.r@gmail.com>
2016-11-30 21:24:41 +0530

    mongodb

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
+var url = process.env.MONGODB;
 
 app.use(express.static(__dirname));
 
-mongoose.connect('mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT + '/mydb', function(err, conn) {
+console.log(process.env);
+
+mongoose.connect(url, function(err, conn) {
 
     if (err) {
         console.log("Failed");
